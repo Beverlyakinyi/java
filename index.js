b88738382d3f90681a96edd287442be7e2187477
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-07 07:15:01 +0300

    added files

---
+++ index.js
@@ -0,0 +1,28 @@
+var a=4
+console.log(a)
+var a=8
+console.log(a)
+
+let b=7
+console.log(b)
+b=9
+console.log(b)
+
+const n=5
+console.log(n)
+
+var a="Beverly"
+console.log(a)
+
+var g=9
+var h=8
+var d=g+h
+console.log(d)
+
+ let petname="Rockynod"
+ 
+console.log(petname)
+let f=2**7
+console.log(f)
+let r=4+4
+console.log(r)
